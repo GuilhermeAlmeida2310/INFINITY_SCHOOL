128b34d4a427e096421dad89db19bfd00f2bb9d0
GuilhermeAlmeida2310 <guilhermehenriquegmil.com@gmail.com>
2026-02-10 12:58:32 -0300

    Provas INFINITY SCHOOL

---
+++ Provas/PYIA Python IA/aula01_prova_pyia_listas_e_tuplas.py
@@ -0,0 +1,8 @@
+# Crie uma lista contendo seis frutas de sua escolha. Depois de ter a lista pronta, converta essa lista em uma tupla. Por fim, exiba o conteúdo da tupla resultante para verificar as frutas que foram armazenadas.
+
+lista = ['maçã', 'banana', 'uva', 'morango', 'pera', 'melância']
+
+tupla = tuple(lista)
+
+print(type(tupla))
+print(tupla)
